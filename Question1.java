dd6622d81ef986eb50151185d85b2a34dd133c5e
DIVYAM TAYAL <divyamtayal18@gmail.com>
2022-09-15 23:14:53 +0530

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+        System.out.println("Question 1");
+        int n1 = 5;
+        int n2 = 10;
+        int sum = n1 + n2;
+        int sub = n2 - n1;
+        System.out.println("Sum of two integer numbers " + n2 + " & " + n1 + " = " + sum);
+        System.out.println("Subtraction of two integer numbers " + n2 + " & " + n1 + " = " + sub);
+        System.out.println();
+
+    }
+}
